4dea081a518ea464c34647ff6e8f6ce731a8dbd6
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2020-06-25 09:39:44 +0900

    'Child側の処理を共通化'

--- server_c1.go
+++ server_c1.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-)
-
-var ServerName = "C1"
-var ResponseCount = 0
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	ResponseCount += 1
-	hostname, _ := os.Hostname()
-
-	fmt.Fprintf(w, "I am server %s on host '%s' (%d times).\n",
-		ServerName, hostname, ResponseCount)
-}
-
 func main() {
-	fmt.Println("Test server", ServerName, "started.")
-
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	run_child("C1", "r1")
 }
